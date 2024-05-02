455a3d75268c65e688aa42aeeed808577c8b6438
esteban2368 <esteban.d2168@gmail.com>
2024-05-01 22:31:46 -0500

    add fonts and edit theme default screens to tailwind

---
+++ fonts/fonts.ts
@@ -0,0 +1,16 @@
+import { Montserrat, Mystery_Quest } from "next/font/google";
+
+
+export const monserrat = Montserrat({
+    weight: ["500","600","700"],
+    subsets: ["latin"],
+    display: "swap"
+})
+
+export const mystery_quest = Mystery_Quest({
+    weight:  ["400"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-mystery-quest"
+}) 
+
