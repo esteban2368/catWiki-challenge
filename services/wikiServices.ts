422ba33eccfad7a20d348359dda9657bf2efc111
esteban2368 <esteban.d2168@gmail.com>
2024-05-25 20:24:57 -0500

    add magament error fetch data

--- services/wikiServices.ts
+++ services/wikiServices.ts
@@ -7,14 +7,18 @@ headers.append('Content-Type', 'application/json')
 headers.append('x-api-key', `${process.env.API_CAT_KEY}`)
 
 export const getBreeds = async() => {
-    const response = await fetch(
-        `${process.env.API_PORT}${process.env.API_CAT_URL}/breeds`,
-        {
-            headers: headers
-        })
-    const result = await response.json()
-
-    return result
+    try {
+        const response = await fetch(
+            `${process.env.API_PORT}${process.env.API_CAT_URL}/breeds`,
+            {
+                headers: headers
+            })
+        const result = await response.json()
+    
+        return result
+    } catch (error:any) {
+        return {error: error.message}
+    }
 }
 
 export const getBreedByID = async (id: string) => {
